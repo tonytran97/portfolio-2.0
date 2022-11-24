3ce0b8332cc3e757bde62ae187faab806684dd29
tonytran97 <tonytran_97@live.com>
2022-11-23 17:45:14 -0800

    + header tag, name

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../styles/nav.css';
 
-export default function ColorSchemesExample() {
+export default function Nav() {
     return (
-        <nav>
-            <ul className='navlinks'>
-                <li><a href="/">About Me</a></li>
-                <li><a href="/">Portfolio</a></li>
-                <li><a href="/">Contact</a></li>
-                <li><a href="/">Resume</a></li>
-            </ul>
-        </nav>
+        <header>
+            <nav>
+                <h3>Hi, I'm Tony Tran</h3>
+                <ul className='navlinks'>
+                    <li><a href="/">About Me</a></li>
+                    <li><a href="/">Portfolio</a></li>
+                    <li><a href="/">Contact</a></li>
+                    <li><a href="/">Resume</a></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
