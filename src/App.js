adbf33ac9a0ace44e6970acddb6b40de6c5f959e
tonytran97 <tonytran_97@live.com>
2022-11-23 19:59:55 -0800

    + aboutme component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './styles/nav.css';
 import Nav from './components/nav';
+import About from './components/about';
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <About />
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Profile from '../assets/images/me.png';
+import '../styles/about.css';
+
+export default function About() {
+    return (
+        <section className='card text-center'>
+            <div className="card-header">
+                About Me
+            </div>
+            <img className="me" src={Profile} alt="me"></img>
+            <p className='card-text'>
+                After devoting a significant amount of my academic career and life to pursing medicine, I have finally discovered my passion for web development and technology.
+                <br />
+                Currently enrolled in the University of Washington Bootcamp to become a full-stack developer.
+                <br />
+                I spend my free time focusing on my web development skills or exercising in the gym.
+            </p>
+            <div className="card-header">
+                A few languages and tools that I am familiar with...
+            </div>
+            <ul className='card-text list'>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>MongoDB</li>
+                <li>Heroku</li>
+                <li>MySQL</li>
+                <li>Bootstrap</li>
+            </ul>
+        </section>
+    );
+}
+
