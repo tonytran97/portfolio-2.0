bce965a4213bf319b7bc3e069a143dd3a093ffa5
tonytran97 <tonytran_97@live.com>
2022-11-28 08:22:09 -0800

    + functional navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import Nav from './components/nav';
 import About from './components/about';
 import Portfolio from './components/portfolio';
 import Footer from './components/footer';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState('about');
+
+  const renderPage = () => {
+    if (currentPage === 'about') {
+      return <About />;
+    }
+    if (currentPage === 'portfolio') {
+      return <Portfolio />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div>
-      <Nav />
-      <About />
-      <Portfolio />
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
       <Footer />
     </div>
   );
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../styles/nav.css';
 
-export default function Nav() {
+export default function Nav({ handlePageChange }) {
     return (
         <header>
             <nav>
                 <h3>Hi, I'm Tony Tran</h3>
                 <ul className='navlinks'>
-                    <li><a href="/">About Me</a></li>
-                    <li><a href="/">Portfolio</a></li>
+                    <li><a href="#about" onClick={() => handlePageChange('about')}>About Me</a></li>
+                    <li><a href="#portfolio" onClick={() => handlePageChange('portfolio')}>Portfolio</a></li>
                     <li><a href="/">Contact</a></li>
                     <li><a href="/">Resume</a></li>
                 </ul>
